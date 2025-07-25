4013eefb774b17a2f38fbb222e38ab50caac7903
MemeKnight <57252398+MrMemeKnight@users.noreply.github.com>
2025-07-25 14:28:32 +0800

    Delete PlayerConfig.cs

--- PlayerConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace MultiLineFishing
-{
-    public class PlayerConfig
-    {
-        public bool Enabled { get; set; } = false;
-        public int ExtraLines { get; set; } = 2; // Number of extra bobbers (default 2, range 1–5)
-    }
-}
